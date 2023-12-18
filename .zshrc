ba73b6b5a9afb32a2ebc6b04c3393c75b87719ee
thong.nguyen5 <thong.nguyen5@mservice.com.vn>
2023-12-18 11:57:44 +0700

    Update zsh plugins

--- .zshrc
+++ .zshrc
@@ -28,7 +28,7 @@ ZSH_THEME="agnoster"
 # zstyle ':omz:update' mode disabled  # disable automatic updates
 # zstyle ':omz:update' mode auto      # update automatically without asking
 # zstyle ':omz:update' mode reminder  # just remind me to update when it's time
-ZSH_DISABLE_COMFIX="true"
+#ZSH_DISABLE_COMFIX="true"
 
 # Uncomment the following line to change how often to auto-update (in days).
 # zstyle ':omz:update' frequency 13
--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ yes Yes | sudo apt install zsh
 echo "Install oh-my-zsh"
 yes Yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo "Install zsh plugins"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+
 echo "Setup zsh profile"
 sudo mv .zshrc ~/
 sudo mv .zshrc_momo ~/
